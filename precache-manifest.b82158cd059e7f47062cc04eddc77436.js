df47d57aaeec8de43841f708f159ab257cca07da
David <sachakove@gmail.com>
2021-12-09 12:09:49 +0200

    Updates

---
+++ precache-manifest.b82158cd059e7f47062cc04eddc77436.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c88b571c7cb3fa1d4d57fe39fe9ce58",
+    "url": "/PplFinder/index.html"
+  },
+  {
+    "revision": "a219be72a74dc7878a67",
+    "url": "/PplFinder/static/js/2.55bd4660.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/PplFinder/static/js/2.55bd4660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb09eea5f978c586792",
+    "url": "/PplFinder/static/js/main.ad9e7d00.chunk.js"
+  },
+  {
+    "revision": "32eb9f5c17181ba0ef19",
+    "url": "/PplFinder/static/js/runtime-main.b6c1fb24.js"
+  }
+]);
